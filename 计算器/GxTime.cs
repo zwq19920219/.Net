bd71f82090726d3e3fe75567152b9e0030eb28b2
zwq19920219 <295188413@qq.com>
2018-11-28 14:38:10 +0800

    模块 应用程序 的初始签入

---
+++ 计算器/GxTime.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace 计算器
+{
+    public class GxTime
+    {
+        public GxTime()
+        {
+
+        }
+
+        public DateTime CurrentTime
+        {
+            get;set;
+        }
+    }
+}
